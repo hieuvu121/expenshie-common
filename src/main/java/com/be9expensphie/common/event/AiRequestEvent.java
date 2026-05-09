def170c22a7be840d8c8df7edcc62aadc7f1561e
hieuvu121 <hieubeobilabi@gmail.com>
2026-05-10 08:54:20 +1000

    add ai type

--- src/main/java/com/be9expensphie/common/event/AiRequestEvent.java
+++ src/main/java/com/be9expensphie/common/event/AiRequestEvent.java
@@ -12,4 +12,5 @@ import lombok.NoArgsConstructor;
 public class AiRequestEvent {
     private String prompt;
     private Long householdId;
+    private AiRequestType type;
 }
---
+++ src/main/java/com/be9expensphie/common/event/AiRequestType.java
@@ -0,0 +1,6 @@
+package com.be9expensphie.common.event;
+
+public enum AiRequestType {
+    PARSE_EXPENSE,
+    GENERATE_SUGGESTION
+}
